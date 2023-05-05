b35a60e7451f3b9f90a91dbedf99ba089a223bc9
ciberdown <teymuri.amin@gmail.com>
2023-05-05 20:21:49 +0330

    text added

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <p className='bg-primary text-danger'>amin teymuri</p>
+    <div className="bg-black w-100 min-vh-100 p-5">
+      <span id='paragraph' className='text-white'><p id='colored' className='border bg-warning d-inline text-black'>L</p>
+      orem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatem ut doloremque harum veniam, repudiandae ex molestias animi praesentium soluta obcaecati recusandae, aperiam laudantium facilis enim quia et non quo ab hic labore minus odio? Ipsam dicta est dolorum, recusandae quo vel minus illum eveniet numquam excepturi a tempora animi laborum necessitatibus autem libero, voluptates molestias alias. Voluptate temporibus nihil debitis quaerat velit placeat quisquam eveniet? Et nihil officia aperiam odio itaque repudiandae optio, facilis rerum dignissimos hic debitis fugit fugiat repellendus commodi amet aspernatur nam accusantium, perferendis asperiores sapiente eveniet cumque velit molestiae soluta? Optio quis sequi repellat non.</span>
+      
     </div>
   );
 }
